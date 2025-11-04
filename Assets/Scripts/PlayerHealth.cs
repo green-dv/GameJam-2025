5d1c9857e63be49f3b0b3b3b5a98831b907a7e9e
NefACN <neptalialanoca106@gmail.com>
2025-11-04 05:11:58 -0400

    Main Menu y configuraciones del Options

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int health;
+    public int maxHealth = 3;
+
+    public SpriteRenderer playerSr;
+    public PlayerController playerController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        if(health <= 0)
+        {
+            health = 0;
+            PlayerDeath();
+        }
+    }
+
+    void PlayerDeath()
+    {
+        playerSr.enabled= false;
+        playerController.enabled= false;
+
+        if(GameController.instance != null)
+        {
+            GameController.instance.GameOver(); 
+        }
+
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<Rigidbody2D>().simulated = false;
+    }
+}
